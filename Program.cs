72a15ed0276f4010a9f9507e383393aca59dac87
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-11 19:10:53 +0300

    task4 completed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает 
+//число (N), а на выходе показывает все чётные 
+//числа от 1 до N.
+
+System.Console.Write("введите число N: ");
+string x = Console.ReadLine();
+int N = int.Parse(x);
+int a = 2;
+while (a < N)
+{
+    System.Console.WriteLine($"{a}, ");
+    a=a+2;
+}
+
